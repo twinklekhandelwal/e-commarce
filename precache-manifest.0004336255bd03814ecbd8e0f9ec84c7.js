0810993d9545834c2bf717a773e08b9170be0436
twinkle <twinkle18@navgurukul.org>
2020-02-14 12:25:03 +0530

    Updates

---
+++ precache-manifest.0004336255bd03814ecbd8e0f9ec84c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d250461848e820dfa56ced78b5c42c",
+    "url": "/E-Commarce-Website.git/index.html"
+  },
+  {
+    "revision": "a37df7496798a13424ba",
+    "url": "/E-Commarce-Website.git/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "da1a32532b5e9e226365",
+    "url": "/E-Commarce-Website.git/static/css/main.9513bd8c.chunk.css"
+  },
+  {
+    "revision": "a37df7496798a13424ba",
+    "url": "/E-Commarce-Website.git/static/js/2.724fee68.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/E-Commarce-Website.git/static/js/2.724fee68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1a32532b5e9e226365",
+    "url": "/E-Commarce-Website.git/static/js/main.2cd4d323.chunk.js"
+  },
+  {
+    "revision": "af79913dca3cf531f661",
+    "url": "/E-Commarce-Website.git/static/js/runtime-main.24a5dff8.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-Commarce-Website.git/static/media/download.9f7e506d.svg"
+  }
+]);
